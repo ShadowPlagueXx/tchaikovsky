623651d28e790d458faad577dfa372b73993485b
ShadowPlagueXx <51781829+ShadowPlagueXx@users.noreply.github.com>
2020-04-05 15:29:52 +0800

    Folium mapping
    Learned quite a bit about choropleth usage and adding it to maps, experimented with mapping on Malaysia, mapping is INACCURATE and not my goal, it is experimental

--- search.py
+++ search.py
@@ -104,34 +104,4 @@ def search(category, types, areas, states, file_name):
     print('total error: ' + str(total_error))
 
 
-# reference: https://ms.wikipedia.org/wiki/Senarai_daerah_bagi_setiap_negeri_di_Malaysia_mengikut_populasi
-list_of_state = ['penang', 'johor', 'kedah', 'kelantan', 'melaka', 'negeri sembilan', 'pahang', 'perak', 'selangor',
-                 'terengganu', 'perlis', 'wilayah persekutuan', 'sabah', 'sarawak']
-list_of_area = [
-    ['timur laut', 'barat daya', 'seberang perai utara', 'seberang perai tengah', 'seberang perai selatan'],
-    ['johor bahru', 'batu pahat', 'kluang', 'kulai', 'muar', 'kota tinggi', 'segamat', 'pontian', 'tangkak',
-     'mersing'],
-    ['sungai petani', 'alor setar', 'kulim', 'kubang pasu', 'baling', 'pendang', 'langkawi', 'yan', 'sik',
-     'padang terap', 'pokok sena', 'bandar baharu'],
-    ['kota bharu', 'pasir mas', 'tumpat', 'bachok', 'tanah merah', 'pasir puteh', 'kuala krai', 'machang', 'gua musang',
-     'jeli', 'lojing'],
-    ['melaka tengah', 'alor gajah', 'jasin'],
-    ['seremban', 'jempol', 'port dickson', 'tampin', 'kuala pilah', 'rembau', 'jelebu'],
-    ['kuantan', 'temerloh', 'bentong', 'maran', 'rompin', 'pekan', 'bera', 'raub', 'jerantut', 'lipis',
-     'cameron highlands'],
-    ['kinta', 'larut matang selama', 'manjung', 'kerian', 'batang padang', 'kuala kangsar', 'perak tengah', 'hulu',
-     'kampar', 'muallim', 'bagan datuk'],
-    ['petaling', 'hulu langat', 'klang', 'gombak', 'kuala langat', 'sepang', 'kuala', 'hulu', 'sabak bernam'],
-    ['kuala', 'kemaman', 'dungun', 'besut', 'marang', 'hulu', 'setiu', 'kuala nerus'],
-    ['arau', 'kaki bukit', 'kangar', 'kuala', 'padang besar', 'simpang empat'],
-    ['kuala lumpur', 'labuan', 'putrajaya'],
-    ['kota kinabalu', 'tawau', 'sandakan', 'lahad datu', 'keningau', 'kinabatangan', 'semporna', 'papar', 'penampang',
-     'beluran', 'tuaran', 'ranau', 'kota belud', 'kudat', 'kota marudu', 'beaufort', 'kunak', 'tenom', 'putatan',
-     'pitas', 'tambunan', 'tongod', 'sipitang', 'nabawan', 'kuala penyu'],
-    ['kuching', 'miri', 'sibu', 'bintulu', 'serian', 'samarahan', 'sri aman', 'marudi', 'betong', 'sarikei', 'kapit',
-     'bau', 'limbang', 'saratok', 'mukah', 'simunjan', 'lawas', 'belaga', 'lundu', 'asajaya', 'daro', 'tatau',
-     'maradong', 'kanowit', 'lubok antu', 'selangau', 'song', 'dalat', 'matu', 'julau', 'pakan', 'padawan']
-]
 
-search('schools', 'school', list_of_area, list_of_state, 'output-school.csv')
-# search('tuition centres', 'tuition_centre', list_of_area, list_of_state, 'output-tc.csv')
